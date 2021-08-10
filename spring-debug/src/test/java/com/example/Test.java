1d80036a36f9e146e8e09b4658add540dbaf6a29
liangTu <clark_tu@163.com>
2021-08-10 13:46:12 +0800

    1 git clone -b 5.2.x 2 ./gradlew :spring-oxm:compileTestJava 3 File -> New -> Project from Existing Sources -> Navigate to directory -> Select build.gradle
    4 create branch 5.2.x_read
    5 new Module spring-debug
    6 modifier spring-debug -> build.gradle  (context beans core aop)
    
        compile(project(":spring-context"))
        compile(project(":spring-beans"))
        compile(project(":spring-core"))
        compile(project(":spring-aop"))

---
+++ spring-debug/src/test/java/com/example/Test.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	@org.junit.jupiter.api.Test
+	public void test(){
+		ApplicationContext xmlApplicationContext
+				= new ClassPathXmlApplicationContext("bean.xml");
+
+		User clark = (User)xmlApplicationContext.getBean("clark");
+		System.out.println(clark);
+
+	}
+
+}
